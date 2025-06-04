5571222068bbcf13de04900c945771b4b914cb1c
dieckman <dieckman@cs.uni-bonn.de>
2025-06-04 16:22:20 +0200

    update shader

--- Graphics/glsl/phong_splatting_fs.glsl
+++ Graphics/glsl/phong_splatting_fs.glsl
@@ -16,6 +16,35 @@ uniform float shininess;     // e.g. 32.0
 uniform float min_color;     // e.g. 32.0
 uniform float max_color;     // e.g. 32.0
 
+float lambertianTerm(vec3 N, vec3 L) {
+    //return max(dot(N, L), 0.0);
+    return abs(dot(N, L)); //somehow my normals are inverted, so I use abs...
+}
+
+float specularTerm(vec3 R, vec3 V, float shininess) {
+    float specAngle = max(dot(R, V), 0.0);
+    return pow(specAngle, shininess);
+}
+
+vec3 PhongShadingFinalColor(vec3 base_color, vec3 N, vec3 L, vec3 view_pos, float shininess, float dist2){
+    // Lambertian (diffuse) term
+    float lambertian = lambertianTerm(N, L);
+
+    // Phong specular term
+    float specular = 0.0;
+    if (lambertian > 0.0) {
+        vec3 V = normalize(-view_pos);   // view direction (camera at origin)
+        vec3 R = reflect(-L, N);                   // perfect reflection
+        specular = specularTerm(R, V, shininess);
+    }
+
+    vec3 ambient = ambient_color * base_color;
+    vec3 diffuse = light_color * (lambertian * base_color) / dist2;
+    vec3 specCol = light_color * (specular * specular_color) / dist2;
+
+    return ambient + diffuse + specCol;
+}
+
 void main() {
     vec3 N = normalize(fs_in.fragNormal);
     vec3 Ldir = light_position - fs_in.fragPosition;
@@ -23,23 +52,22 @@ void main() {
     vec3 L = normalize(Ldir);
     vec3 C = (fs_in.fragColor - min_color) / (max_color - min_color); // normalize color
 
-    // Lambertian (diffuse) term
-    float lambertian = max(dot(N, L), 0.0);
-    lambertian = abs(dot(N, L));
+/*    // Lambertian (diffuse) term
+    float lambertian = lambertianTerm(N, L);
 
     // Phong specular term
     float specular = 0.0;
     if (lambertian > 0.0) {
         vec3 V = normalize(-fs_in.fragPosition);   // view direction (camera at origin)
         vec3 R = reflect(-L, N);                   // perfect reflection
-        float specAngle = max(dot(R, V), 0.0);
-        specular = pow(specAngle, shininess);
+        specular = specularTerm(R, V, shininess);
     }
 
     vec3 ambient = ambient_color * C;
     vec3 diffuse = light_color * (lambertian * C) / dist2;
     vec3 specCol = light_color * (specular * specular_color) / dist2;
 
-    vec3 finalColor = ambient + diffuse + specCol;
+    vec3 finalColor = ambient + diffuse + specCol;*/
+    vec3 finalColor = PhongShadingFinalColor(C, N, L, fs_in.fragPosition, shininess, dist2);
     outColor = vec4(finalColor, 1.0);
 }
