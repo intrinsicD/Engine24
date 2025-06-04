99c03dfc9688c95b23659a4a8f4ebe676fbdbcfd
intrinsicD <intrinsicd@posteo.de>
2025-06-04 23:41:35 +0200

    using slang now...Todos

--- Graphics/ModuleShaderSlang.h
+++ Graphics/ModuleShaderSlang.h
@@ -5,4 +5,27 @@
 #ifndef MODULESHADERSLANG_H
 #define MODULESHADERSLANG_H
 
+#include "Module.h"
+
+namespace Bcg {
+    class ModuleShaderSlang : public Module {
+    public:
+        ModuleShaderSlang() : Module("ModuleShaderSlang") {
+
+        }
+
+        void activate() override;
+
+        void deactivate() override;
+
+        void update() override;
+
+        void render_menu() override;
+
+        void render_gui() override;
+
+        //static methods for shader compilation and management
+    };
+}
+
 #endif //MODULESHADERSLANG_H
--- src/Graphics/ModuleShaderSlang.cpp
+++ src/Graphics/ModuleShaderSlang.cpp
@@ -1,3 +1,63 @@
 //
 // Created by alex on 6/4/25.
 //
+
+#include "ModuleShaderSlang.h"
+#include "Engine.h"
+#include "Logger.h"
+#include <slang.h>
+#include <slang-com-helper.h>
+#include <slang-com-ptr.h>
+
+
+
+namespace Bcg {
+    struct SlangData {
+        Slang::ComPtr<slang::IGlobalSession> global_session;
+        Slang::ComPtr<slang::ISession> compile_session;
+        slang::SessionDesc session_desc = {};
+        std::vector<slang::TargetDesc> targets;  // array of target descriptors
+
+        // We also need a place to hold the linked program so that
+        // Slang reflection or code generation can reference it later:
+        Slang::ComPtr<slang::IModule>       module;
+        Slang::ComPtr<slang::IComponentType> composite_component;
+        Slang::ComPtr<slang::IComponentType> linked_program;
+    };
+
+    void ModuleShaderSlang::activate() {
+        auto &slang = Engine::Context().emplace<SlangData>();
+        if (!slang::createGlobalSession(slang.global_session.writeRef())) {
+            Log::Error("{}: Failed to create global session", name);
+            return;
+        }
+        //Whats missing?
+
+    }
+
+    void ModuleShaderSlang::deactivate() {
+        if (base_deactivate()) {
+            Slang::ComPtr<slang::IGlobalSession> global_session;
+            Slang::ComPtr<slang::ISession> compile_session;
+            slang::SessionDesc session_desc = {};
+            std::vector<slang::TargetDesc> targets;  // array of target descriptors
+
+            // We also need a place to hold the linked program so that
+            // Slang reflection or code generation can reference it later:
+            Slang::ComPtr<slang::IModule>       module;
+            Slang::ComPtr<slang::IComponentType> composite_component;
+            Slang::ComPtr<slang::IComponentType> linked_program;
+
+            auto &slangData = Engine::Context().get<SlangData>();
+        }
+    }
+
+    void ModuleShaderSlang::update() {
+    }
+
+    void ModuleShaderSlang::render_menu() {
+    }
+
+    void ModuleShaderSlang::render_gui() {
+    }
+}
